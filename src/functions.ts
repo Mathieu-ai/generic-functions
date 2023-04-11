04c1c160fd8980257da5b68824f5b335688894f1
Mathieu-ai <mahieu.lievre@efrei.net>
2023-04-11 11:02:23 +0200

    feat: v.0.3.5:             - getUnique

--- src/functions.ts
+++ src/functions.ts
@@ -1,5 +1,6 @@
 import request from "axios";
 import { decodeHTML } from 'entities';
+import objectHash from 'object-hash';
 import dayjs, { Dayjs } from "dayjs";
 import {
     ExtractionResult,
@@ -27,7 +28,7 @@ const {
     @param {String} env
     @return Object | data
     @example
-        _API("http://localhost:8080/members")
+        api("http://localhost:8080/members")
 */
 export async function api(env: string): Promise<object[]> {
     const res = await request({
@@ -119,7 +120,6 @@ export function getInitials(str: string): string {
     }, []);
     return initials.join("");
 }
-
 
 /**
     Convert string without the html breaklines
@@ -663,4 +663,63 @@ export function compareTypes(
 */
 export function convertHtmlEntities(str: string): string {
     return decodeHTML(str)
+}
+
+/**
+    Get the value of an object by its path
+    @param obj - The object to traverse
+    @param path - The path to the value
+    @returns The value at the specified path, null if it doesn't exist
+    @example
+        const obj = { foo: { bar: 'baz' } };
+        const path = ['foo', 'bar'];
+        const value = getObjectValueByPath(obj, path); // 'baz'
+*/
+export function getObjectValueByPath(obj: any, path: string[]): any {
+    return path.reduce((o, key) => {
+        if (o && Array.isArray(o)) {
+            return o.map((item) => getObjectValueByPath(item, [key])).flat();
+        }
+        return (o && o[key]) ? o[key] : null;
+    }, obj);
+}
+
+/**
+    Get an array of unique values from an array of strings or objects
+    @param data - The array of strings or objects to filter
+    @param field - Optional. specified field
+    @returns An array of unique values.
+    @example
+        const data = [
+            { id: 1, name: 'Alice' },
+            { id: 2, name: 'Bob' },
+            { id: 3, name: 'Alice' },
+            { id: 4, name: 'Charlie' },
+        ];
+        const uniqueByName = getUnique(data, 'name'); 
+        // [{ id: 1, name: 'Alice' }, { id: 2, name: 'Bob' }, { id: 4, name: 'Charlie' }]
+*/
+export function getUnique(data: (string | object)[], field?: string) {
+    const fieldPath = field ? field.split('.') : [];
+    const uniqueValues: (string | object)[] = [];
+    const seen = new Set<string>();
+
+    for (const value of data) {
+        const uniqueValue = getObjectValueByPath(value, fieldPath);
+        if (Array.isArray(uniqueValue)) {
+            const uniqueValueHashes = uniqueValue.map((uv) => objectHash(uv));
+            if (!uniqueValueHashes.some((hash) => seen.has(hash))) {
+                uniqueValueHashes.forEach((hash) => seen.add(hash));
+                uniqueValues.push(value);
+            }
+        } else {
+            const uniqueValueHash = objectHash(uniqueValue);
+            if (uniqueValue && !seen.has(uniqueValueHash)) {
+                seen.add(uniqueValueHash);
+                uniqueValues.push(value);
+            }
+        }
+    }
+
+    return uniqueValues;
 }
