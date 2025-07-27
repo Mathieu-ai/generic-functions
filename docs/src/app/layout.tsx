8a1b658c73b50b8fef7849aa7b483c6f58dfc494
Mathieu-ai <mathieu.lievre.pro@outlook.com>
2025-07-27 19:43:29 +0200

    test: - is signed 2 ?

--- docs/src/app/layout.tsx
+++ docs/src/app/layout.tsx
@@ -28,7 +28,7 @@ export const metadata: Metadata = {
   },
 };
 
-export default function RootLayout({
+export default function RootLayout ({
   children,
 }: Readonly<{
   children: React.ReactNode;
